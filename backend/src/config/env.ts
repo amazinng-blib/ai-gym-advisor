411a388d24573210e3705feb9eed73702c4e8d4a
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2026-05-28 00:00:44 +0100

    fix: hide db secrets

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -23,3 +25,15 @@ export const DBEnviroment = {
     dialect: 'postgres',
   },
 };
+
+// convert to JSON string
+// const json = JSON.stringify(DBEnviroment, null, 2);
+
+// // output path (Sequelize expects config.json usually here)
+// // const outputPath = path.join(__dirname, '../src/config/config.json');
+// const outputPath = path.join(__dirname, './config.json');
+
+// // write file
+// fs.writeFileSync(outputPath, json, 'utf-8');
+
+// console.log('✅ config.json generated successfully!');
